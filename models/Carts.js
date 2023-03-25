352822acfa43531a0afd420ae679a164525762cc
Numby86 <numby86@gmail.com>
2023-03-25 10:30:14 +0100

    feat(JOBI)change model cart

--- models/Carts.js
+++ models/Carts.js
@@ -2,17 +2,10 @@ const mongoose = require('mongoose');
 
 const cartSchema = new mongoose.Schema(
     {
-        name: { type: String, required: true, unique: true },
-        image: { type: String, required: true },
-        price: { type: Number, required: true, min: [1, "el precio debe ser mayor de 1 siempre"]},
-        category: { type: [String], 
-                    enum: {
-                    values: ["libros", "juguetes", "ropa", "videojuegos"],
-                    message: "Esta no es una categoria de nuestros productos web"
-                } },
-        stock: { type: Number, required: true},
-        type: [String],
-        products: [{ type: mongoose.Types.ObjectId, ref: 'Product' } ],
+        products: [{ type: {
+            id: mongoose.Types.ObjectId,
+            stock: Number
+        }, ref: 'Product' } ],
         users: [{ type: mongoose.Types.ObjectId, ref: 'User' }]
     },
     {
